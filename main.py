f1c4ffd6f2dbc1d4a167ef084a28c14ef952eaa3
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-11 23:34:26 -0400

    added generated directory to gitignore and created stats.py to create better structure

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def count_words(text):
-    words = text.split()
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    message = f"{count_words(text)} words found in the document"
+    print(message)
     
 main()
