39eff7f545f26a4e4b23caa3b2ceb5bd3a7ebe30
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-13 19:24:44 -0400

    started work in new stats function

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from stats import count_words, count_letters
+from stats import count_words, count_letters, sort_letters
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    message = f"{count_words(text)} words found in the document"
+    path_book = "books/frankenstein.txt"
+    text = get_book_text(path_book)
+    words = count_words(text)
+    letters = count_letters(text)
+    sorted_letters = sort_letters(letters)
+
+    message = "============ BOOKBOT ============"
+    message += f"\nAnalyzing book found at {path_book}..."
+    message += "\n----------- Word Count ----------"
+    message += f"\nFound {words} total words"
+    message += "\n--------- Character Count -------"
+
+    for letter in sorted_letters:
+        if letter["char"].isalpha():
+            message += f"\n{letter["char"]}: {letter["num"]}"
+
+    message += "\n============= END ==============="
+
     print(message)
-    print(count_letters(text))    
+
+
 main()
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+
 def count_letters(text):
     lower_text = text.lower()
     letters = {}
@@ -11,3 +12,16 @@ def count_letters(text):
         else:
             letters[letter] = 1
     return letters
+
+
+def sort_on(dic):
+    return dic["num"]
+
+
+def sort_letters(letters):
+    sorted_letters = []
+    for k, v in letters.items():
+        letter = {"char": k, "num": v}
+        sorted_letters.append(letter)
+    sorted_letters.sort(reverse=True, key=sort_on)
+    return sorted_letters
